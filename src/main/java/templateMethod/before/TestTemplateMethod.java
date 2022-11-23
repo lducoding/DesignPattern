60e35d1d14761e5f61793f43e5e819cac0d77d32
lducoding <lducoding@gmail.com>
2022-11-23 19:20:17 +0900

    template method 시행전

---
+++ src/main/java/templateMethod/before/TestTemplateMethod.java
@@ -0,0 +1,27 @@
+package templateMethod.before;
+
+public class TestTemplateMethod {
+
+  public static void main(String[] args) {
+    AICar myCar = new AICar();
+    myCar.startCar();
+    myCar.drive();
+    myCar.stop();
+    myCar.turnOff();
+    System.out.println("*********************");
+
+    ManualCar herCar = new ManualCar();
+    herCar.startCar();
+    herCar.drive();
+    herCar.stop();
+    herCar.turnOff();
+    System.out.println("*********************");
+
+    HybridCar yourCar = new HybridCar();
+    yourCar.startCar();
+    yourCar.drive();
+    yourCar.stop();
+    yourCar.turnOff();
+    System.out.println("*********************");
+  }
+}
