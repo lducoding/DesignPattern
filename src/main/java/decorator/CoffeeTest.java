2c3cb2304a367e4e13501534e7a73c14696ee155
lducoding <lducoding@gmail.com>
2022-12-14 18:50:54 +0900

    decorator 설명 추가

--- src/main/java/decorator/CoffeeTest.java
+++ src/main/java/decorator/CoffeeTest.java
@@ -1,5 +1,12 @@
 package decorator;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.StringBufferInputStream;
+
 public class CoffeeTest {
 
   public static void main(String[] args) {
@@ -11,5 +18,8 @@ public class CoffeeTest {
 
     Coffee mocha = new Mocha(latte);
     mocha.brewing();
+
+    // java.io 패키지에 InputStream 은 Coffee 와 같은 추상클래스
+    // FileInputStream 이 Decorator 클래스
   }
 }
